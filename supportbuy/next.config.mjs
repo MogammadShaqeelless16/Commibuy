2d5fb55e634e71946a070c7101b3cd23f6d9b0cd
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-09-13 21:52:22 +0000

    update changes

--- supportbuy/next.config.mjs
+++ supportbuy/next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolve the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
-    reactStrictMode: true,
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+export default nextConfig;
